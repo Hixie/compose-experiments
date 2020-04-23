470fec2f5ede0188eb05ce07d26066c8eb58e92a
Ian Hickson <ian@hixie.ch>
2020-04-23 14:39:05 -0700

    Crash?

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # compose-experiments
-Wherein Hixie plays with JetPack Compose
+
+Wherein Hixie plays with JetPack Compose.
+
+This repo is just a convenient place to store code so that I can share it with colleagues.
---
+++ counter/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.*
+import androidx.ui.core.setContent
+import androidx.ui.foundation.Text
+import androidx.ui.layout.Column
+import androidx.ui.material.Button
+import androidx.ui.material.Card
+import androidx.ui.material.MaterialTheme
+import androidx.ui.tooling.preview.Preview
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Column {
+                    Greeting("Alice")
+                    Greeting("Bob")
+                    Greeting("Charley")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    //var x = state { 0 }
+    //Card {
+    //    Button(onClick = { /*x.value += 1*/ }) {
+            Text(text = "Hello $name ${0/*x.value*/}!")
+    //    }
+    //}
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MaterialTheme {
+        Greeting("Android")
+    }
+}
